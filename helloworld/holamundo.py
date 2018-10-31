1fe28b6bf96b6e8306d1d28f3362814f5c65ccd1
josemiteba <josemi.teba@hotmail.com>
2018-10-31 10:37:29 +0100

    Crear mensaje en Elmah.io para el dashboard Savia

--- helloworld/holamundo.py
+++ helloworld/holamundo.py
@@ -1,4 +1,19 @@
 # coding=utf-8
-print("Hola mundo!")
-print("Este es mi primer despliegue automático")
-print("Haciendo otra prueba")
+import http.client
+
+conn = http.client.HTTPConnection("api,elmah,io")
+
+payload = "{\r\n    \"title\": \"Este es mi mensaje de prueba en Savia 2\"\r\n}"
+
+headers = {
+    'Content-Type': "application/json",
+    'cache-control': "no-cache",
+    'Postman-Token': "b806fc9f-edee-4371-a0c9-39dd69a11149"
+    }
+
+conn.request("POST", "v3,messages,148620e1-beea-4c03-9b0f-56c75e526d61", payload, headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
